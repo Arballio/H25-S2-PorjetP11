17616120af28e7a134abd5e8d143e9c262237ec8
tfraude <tristanfraud@icloud.com>
2025-02-26 20:37:42 -0500

    V5 Accéléro

---
+++ lib/Accelero/Accelero.cpp
@@ -0,0 +1,27 @@
+#include <Accelero.h>
+
+void movementdetec()
+{
+    pinMode(groundpin, OUTPUT);
+    pinMode(powerpin, OUTPUT);
+    digitalWrite(groundpin, LOW);
+    digitalWrite(powerpin, HIGH);
+
+    int Xpin = analogRead(xpin);
+    int Ypin = analogRead(ypin);
+    int Zpin = analogRead(zpin);
+    if (Zpin > 300)
+    {
+        Serial.print("movement detected");
+        Serial.print("\n");
+        Serial.print(Xpin);
+        Serial.print("\n");
+        Serial.print(Ypin);
+        Serial.print("\n");
+        Serial.print(Zpin);
+        Serial.print("\n");
+        delay(100);
+    }
+
+   
+}
---
+++ lib/Accelero/Accelero.h
@@ -0,0 +1,9 @@
+#include <Arduino.h>
+#include <stdio.h>
+
+const int groundpin = 18;
+const int powerpin = 19;
+const int xpin = A13;
+const int ypin = A12;
+const int zpin = A11;
+void movementdetec();
