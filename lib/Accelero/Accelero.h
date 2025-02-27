e6e2724599e99d774f433f731d152e5355efca53
tfraude <tristanfraud@icloud.com>
2025-02-27 10:13:26 -0500

    V5

--- lib/Accelero/Accelero.h
+++ lib/Accelero/Accelero.h
@@ -1,10 +1,13 @@
 #include <Arduino.h>
 #include <stdio.h>
+#include "bargraph.h"
 
 const int groundpin = 18;
 const int powerpin = 19;
 const int xpin = A13;
 const int ypin = A12;
 const int zpin = A11;
-
+int Xpin = analogRead(xpin);
+int Ypin = analogRead(ypin);
+int Zpin = analogRead(zpin); 
 void movementdetec();
